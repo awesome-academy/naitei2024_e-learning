c91596e5d9b0d5992b02973781606b98fe28557b
ctpanh <ctpanh03@gmail.com>
2024-08-05 22:10:09 +0700

    feat: 78276

--- src/services/lesson.service.ts
+++ src/services/lesson.service.ts
@@ -1,7 +1,9 @@
+import { StudentLesson } from '../entity/student_lesson.entity';
 import { AppDataSource } from '../config/data-source';
 import { Lesson } from '../entity/lesson.entity';
 
 const lessonRepository = AppDataSource.getRepository(Lesson);
+const studentLessonRepository = AppDataSource.getRepository(StudentLesson);
 
 export const getLessonList = async (userId: string, courseId: string) => {
   const lessons = await lessonRepository.find({
@@ -28,3 +30,16 @@ export const getLessonList = async (userId: string, courseId: string) => {
 
 export const getLessonById = async (id: string) =>
   await lessonRepository.findOne({ where: { id } });
+
+export const markDoneLesson = async (id: string) => {
+  const studentLesson = await studentLessonRepository.findOne({
+    where: { id },
+    relations: ['lesson'],
+  });
+  if (!studentLesson) {
+    return;
+  }
+  studentLesson.done = true;
+  await studentLessonRepository.save(studentLesson);
+  return studentLesson;
+};
